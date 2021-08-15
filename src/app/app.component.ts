4765f315b9b2da245a21b97f560c09147832a62c
Nebiros56 <carlosrengifo569@gmail.com>
2021-08-15 16:25:42 -0500

    title 1 modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-versioned-project';
+  title = 'Simple Versioned Project';
 }
